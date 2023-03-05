fc074f8601305974d651620bf7fba0ca288f2e0e
mangofunky <mangofunky@hotmail.com>
2023-03-05 13:59:42 +0000

    instrument added Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,11 +26,6 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 #from aws_xray_sdk.core import xray_recorder
 #from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-# Rollbar -----
-#import rollbar
-#import rollbar.contrib.flask
-from flask import got_request_exception
-
 #HoneyComb -------------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -47,7 +42,31 @@ import watchtower
 import logging
 from time import strftime
 
+# ROLLBAR -------
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 app = Flask(__name__)
+
+# ROLLBAR -------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 # Configuring Logger to Use CloudWatch
 #LOGGER = logging.getLogger(__name__)
@@ -85,6 +104,12 @@ cors = CORS(
 #    timestamp = strftime('[%Y-%b-%d %H:%M]')
 #    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #    return response
+
+# Rollbar endpoint
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World! WOW', 'warning')
+    return "Hello World! WOW!!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -26,15 +26,10 @@ class UserActivities:
       model['data'] = results
     # xray ----
     #  subsegment = xray_recorder.begin_subsegment('mock-data')
-      # xray ---
     #  dict = {
     #    "now": now.isoformat(),
     #    "results-size": len(model['data'])
     #  }
     #  subsegment.put_metadata('key', dict, 'namespace')
-    #  xray_recorder.end_subsegment()
-    #finally:  
-    #  # Close the segment
-    #  xray_recorder.end_subsegment()
 
     return model
