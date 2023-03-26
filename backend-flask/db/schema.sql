938e80ea5b4dd18b5666043bd0ea913672dd41d1
mangofunky <mangofunky@hotmail.com>
2023-03-26 17:06:20 +0000

    implemented dynamodb schema-load, scan, get-conversation, list conversation scripts

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,46 +1,25 @@
--- https://www.postgresql.org/docs/current/uuid-ossp.html
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-DROP TRIGGER IF EXISTS trig_users_updated_at ON users;
-DROP TRIGGER IF EXISTS trig_activities_updated_at ON activities;
-
-DROP FUNCTION IF EXISTS func_updated_at();
-CREATE FUNCTION func_updated_at()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = now();
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-
-CREATE TRIGGER trig_users_updated_at 
-BEFORE UPDATE ON users 
-FOR EACH ROW EXECUTE PROCEDURE func_updated_at();
-CREATE TRIGGER trig_activities_updated_at 
-BEFORE UPDATE ON activities 
-FOR EACH ROW EXECUTE PROCEDURE func_updated_at();
-
--- forcefully drop our tables if they already exist
-DROP TABLE IF EXISTS public.users cascade;
+DROP TABLE IF EXISTS public.users;
 DROP TABLE IF EXISTS public.activities;
 
+
 CREATE TABLE public.users (
-  uuid UUID default uuid_generate_v4() primary key,
-  display_name text,
-  email text,
-  handle text,
-  cognito_user_id text,
-  created_at timestamp default current_timestamp NOT NULL
+  uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  display_name text NOT NULL,
+  handle text NOT NULL,
+  email text NOT NULL,
+  cognito_user_id text NOT NULL,
+  created_at TIMESTAMP default current_timestamp NOT NULL
 );
 
 CREATE TABLE public.activities (
-  uuid UUID default uuid_generate_v4() primary key,
-  user_uuid UUID REFERENCES public.users(uuid) NOT NULL,
+  uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_uuid UUID NOT NULL,
   message text NOT NULL,
-  replies_count integer default 0,
-  reposts_count integer default 0,
-  likes_count integer default 0,
+  replies_count integer DEFAULT 0,
+  reposts_count integer DEFAULT 0,
+  likes_count integer DEFAULT 0,
   reply_to_activity_uuid integer,
-  expires_at timestamp,
-  created_at timestamp default current_timestamp NOT NULL
+  expires_at TIMESTAMP,
+  created_at TIMESTAMP default current_timestamp NOT NULL
 );
