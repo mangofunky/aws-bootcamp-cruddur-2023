09952fd8da8b5dac7bb5fe87917a819904e619b6
mangofunky <mangofunky@hotmail.com>
2023-04-09 15:02:16 +0000

    dynamoDB final updates, new files

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -4,6 +4,7 @@ VALUES
   -- we can grab the cognito_user_id manually from the Congito Console
   ('andrew@exampro.co', 'Andrew Brown', 'andrewbrown' ,'7659c102-7ad4-44bd-bdc3-208f934d1f41'),
   ('mangofunky@hotmail.com', 'Giuseppe Mangano', 'mangofunky' ,'MOCK');
+  ('londomollari@hotmail.com', 'Londo Mollari', 'mollari' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
