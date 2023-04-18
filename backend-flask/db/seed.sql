7f35ce6d58024faea34569246390ea23a945f01e
mangofunky <mangofunky@hotmail.com>
2023-04-18 13:33:52 +0000

    fixed pathing for scripts

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -4,7 +4,9 @@ VALUES
   -- we can grab the cognito_user_id manually from the Congito Console
   ('andrew@exampro.co', 'Andrew Brown', 'andrewbrown' ,'7659c102-7ad4-44bd-bdc3-208f934d1f41'),
   ('mangofunky@hotmail.com', 'Giuseppe Mangano', 'mangofunky' ,'MOCK'),
-  ('londomollari@hotmail.com', 'Londo Mollari', 'mollari' ,'MOCK');
+  ('londomollari@hotmail.com', 'Londo Mollari', 'mollari' ,'MOCK'),
+  ('bayko@hotmail.com', 'Andrew Bayko', 'bayko' ,'MOCK'),
+  ('celestialsound@hotmail.com', 'Celectial Sound', 'sound' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
