5deb2462c09099272b66e45576475b91f0cc5b4a
mangofunky <mangofunky@hotmail.com>
2023-04-21 17:20:46 +0000

    tidied up code in thumbing-serverless

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -31,10 +31,25 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
 
     //const bucket = this.createBucket(bucketName);
     const bucket = this.importBucket(bucketName);
+
+    //create a lambda
     const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
+
+    //create sns topic subscription
+    const snsTopic = this.createSnsTopic(topicName);
+    this.createSnsSubscription(snsTopic,webhookUrl);
+    
+    //add S3 event notifications
     this.createS3NotifyToLambda(folderInput,lambda,bucket);
+    this.createS3NotifyToSns(folderOutput,snsTopic,bucket);
+    
+    //create policies
     const s3ReadWritePolicy = this.createPolicyBucketAccess(bucket.bucketArn);
+    const snsPublishPolicy = this.createPolicySnSPublish(snsTopic.topicArn);
+    
+    //attach policies for permissions
     lambda.addToRolePolicy(s3ReadWritePolicy);
+    lambda.addToRolePolicy(snsPublishPolicy);
 
   } 
     createBucket(bucketName: string): s3.IBucket {
@@ -87,4 +102,36 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       });
       return s3ReadWritePolicy;
     }
+    createSnsTopic(topicName: string): sns.ITopic{
+      const logicalName = "Topic";
+      const snsTopic = new sns.Topic(this, logicalName, {
+        topicName: topicName
+      });
+      return snsTopic;
+    }
+    createSnsSubscription(snsTopic: sns.ITopic, webhookUrl: string): sns.Subscription {
+      const snsSubscription = snsTopic.addSubscription(
+        new subscriptions.UrlSubscription(webhookUrl)
+      )
+      return snsSubscription;
+    }
+    createS3NotifyToSns(prefix: string, snsTopic: sns.ITopic, bucket: s3.IBucket): void {
+      const destination = new s3n.SnsDestination(snsTopic)
+      bucket.addEventNotification(
+        s3.EventType.OBJECT_CREATED_PUT, 
+        destination,
+        {prefix: prefix}
+      );
+    }
+      createPolicySnSPublish(topicArn: string){
+        const snsPublishPolicy = new iam.PolicyStatement({
+          actions: [
+            'sns:Publish',
+          ],
+          resources: [
+            topicArn
+          ]
+        });
+        return snsPublishPolicy;
+      }
 }
