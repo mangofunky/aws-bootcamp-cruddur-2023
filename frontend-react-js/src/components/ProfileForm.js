80a72c074840ffe816461bf0583c5cac79b4d23d
mangofunky <mangofunky@hotmail.com>
2023-04-26 13:44:38 +0000

    amended a few lines in profileform.js, db setup amd gitpod

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -77,7 +77,7 @@ export default function ProfileForm(props) {
       await getAccessToken()
       const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
-        method: "POST",
+        method: "PUT",
         headers: {
           'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
