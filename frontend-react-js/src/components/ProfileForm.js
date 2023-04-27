280cf51c6461f793af65de9bc6615351c5344b99
mangofunky <mangofunky@hotmail.com>
2023-04-27 20:39:31 +0000

    cannot get the signedurl to work

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -4,71 +4,77 @@ import process from 'process';
 import {getAccessToken} from 'lib/CheckAuth';
 
 export default function ProfileForm(props) {
-  const [bio, setBio] = React.useState('');
-  const [displayName, setDisplayName] = React.useState('');
+  const [bio, setBio] = React.useState("");
+  const [displayName, setDisplayName] = React.useState("");
 
   React.useEffect(()=>{
-    setBio(props.profile.bio || '');
+    setBio(props.profile.bio || "");
     setDisplayName(props.profile.display_name);
   }, [props.profile])
 
-  const s3uploadkey = async (extension)=> {
-    console.log('ext',extension)
+    const s3uploadKey = async (extension) => {
+    console.log("ext", extension);
     try {
-      const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
-      await getAccessToken()
-      const access_token = localStorage.getItem("access_token")
+      const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`;
+      await getAccessToken();
+      const access_token = localStorage.getItem("access_token");
       const json = {
-        extension: extension
-      }
+        extension: extension,
+      };
       const res = await fetch(gateway_url, {
-        method: "POST",
         body: JSON.stringify(json),
+        method: "POST",
         headers: {
-          'Origin': process.env.REACT_APP_FRONTEND_URL,
-          'Authorization': `Bearer ${access_token}`,
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
+          Authorization: `Bearer ${access_token}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+
       let data = await res.json();
       if (res.status === 200) {
-        return data.url
+        return data.url;
       } else {
-        console.log(res)
+        console.log(res);
       }
     } catch (err) {
       console.log(err);
     }
-  }
-  const s3upload = async (event)=> {
-    console.log('event',event)
-    const file = event.target.files[0]
-    const filename = file.name
-    const size = file.size
-    const type = file.type
-    const preview_image_url = URL.createObjectURL(file)
-    console.log(filename,size,type)
-    const fileparts = filename.split('.')
-    const extension = fileparts[fileparts.length-1]
-    const presignedurl = await s3uploadkey(extension)
+  };
+
+
+  const s3upload = async (event) => {
+    const file = event.target.files[0];
+
+    const filename = file.name;
+    const size = file.size;
+    const type = file.type;
+    const preview_image_url = URL.createObjectURL(file);
+    const fileparts = filename.split(".");
+    const extension = fileparts[fileparts.length - 1];
+    const presignedurl = await s3uploadKey(extension);
+
+    console.log("presignedurl", presignedurl);
+
     try {
-      console.log('s3upload')
-      const res = await fetch(presignedurl, {
+      const backend_url = `${presignedurl}`;
+      const res = await fetch(backend_url, {
         method: "PUT",
         body: file,
         headers: {
-          'Content-Type': type
-      }})
+          "Content-Type": type
+        },
+      });
+
       if (res.status === 200) {
-        
+        console.log("uploaded");
       } else {
-        console.log(res)
+        console.log(res);
       }
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   const onsubmit = async (event) => {
     event.preventDefault();
