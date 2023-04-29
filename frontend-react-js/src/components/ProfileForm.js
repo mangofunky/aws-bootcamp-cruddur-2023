27e0556fae941e59454b65998847e2305850a032
mangofunky <mangofunky@hotmail.com>
2023-04-29 16:01:27 +0100

    implemented later code to see if it works

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -12,23 +12,88 @@ export default function ProfileForm(props) {
     setDisplayName(props.profile.display_name);
   }, [props.profile]);
 
-  const s3upload = async (event) => {
-    try {
-      console.log("S3 upload")
-      const backend_url = "https://gw1ziu9o3a.execute-api.us-east-1.amazonaws.com/avatars/key_upload"
-      await getAccessToken()
-      const access_token = localStorage.getItem("access_token")
-      const res = await fetch(backend_url, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        }})
-      let data = await res.json();
+//   const s3uploadkey = async (event) => {
+//     try {
+//       console.log("S3 upload")
+//       const backend_url = "https://gw1ziu9o3a.execute-api.us-east-1.amazonaws.com/avatars/key_upload"
+//       await getAccessToken()
+//       const access_token = localStorage.getItem("access_token")
+//       const res = await fetch(backend_url, {
+//         method: "POST",
+//         headers: {
+//           Authorization: `Bearer ${access_token}`,
+//           Accept: "application/json",
+//           "Content-Type": "application/json"
+//         }})
+//       let data = await res.json();
       
+//       if (res.status === 200) {
+//         console.log("presigned url", data)
+//       } else {
+//         console.log(res);
+//       }
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+
+  const s3uploadkey = async (extension)=> {
+    console.log("ext", extension)
+    try {
+      const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token");
+      const json = {
+        extension: extension
+      };
+      const res = await fetch(gateway_url, {
+        method: "POST",
+        body: JSON.stringify(json),
+        headers: {
+          'Origin': process.env.REACT_APP_FRONTEND_URL,
+          'Authorization': `Bearer ${access_token}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+      console.log('GATEWAY_URL',res)
+      let data = await res.json();
       if (res.status === 200) {
-        console.log("presigned url", data)
+        return data.url
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  const s3upload = async (event) => {
+    console.log("event", event)
+    const file = event.target.files[0]
+    const filename = file.name
+    const size = file.size
+    const type = file.type
+    const preview_image_url = URL.createObjectURL(file)
+    console.log(
+      "filename :",filename,
+      "size :", size,
+      "type :", type,
+      "preview_image_url :", preview_image_url
+    );
+    const fileparts = filename.split('.');
+    const extension = fileparts[fileparts.length - 1];
+    const presignedurl = await s3uploadkey(extension)
+    try {
+      console.log("s3upload");
+      const res = await fetch(presignedurl, {
+        method: "PUT",
+        body: file,
+        headers: {
+          "Content-Type": type
+        }
+      })
+      if (res.status === 200) {
       } else {
         console.log(res);
       }
@@ -36,71 +101,6 @@ export default function ProfileForm(props) {
       console.log(err);
     }
   };
-
-
-  // const s3uploadkey = async (extension)=> {
-  //   console.log("ext", extension)
-  //   try {
-  //     const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
-  //     await getAccessToken()
-  //     const access_token = localStorage.getItem("access_token");
-  //     const json = {
-  //       extension: extension
-  //     };
-  //     const res = await fetch(gateway_url, {
-  //       method: "POST",
-  //       body: JSON.stringify(json),
-  //       headers: {
-  //         'Origin': process.env.REACT_APP_FRONTEND_URL,
-  //         'Authorization': `Bearer ${access_token}`,
-  //         'Accept': 'application/json',
-  //         'Content-Type': 'application/json'
-  //       }
-  //     })
-  //     console.log('GATEWAY_URL',res)
-  //     let data = await res.json();
-  //     if (res.status === 200) {
-  //       return data.url
-  //     } else {
-  //       console.log(res)
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
-  // const s3upload = async (event) => {
-  //   console.log("event", event)
-  //   const file = event.target.files[0]
-  //   const filename = file.name
-  //   const size = file.size
-  //   const type = file.type
-  //   const preview_image_url = URL.createObjectURL(file)
-  //   console.log(
-  //     "filename :",filename,
-  //     "size :", size,
-  //     "type :", type,
-  //     "preview_image_url :", preview_image_url
-  //   );
-  //   const fileparts = filename.split('.');
-  //   const extension = fileparts[fileparts.length - 1];
-  //   const presignedurl = await s3uploadkey(extension)
-  //   try {
-  //     console.log("s3upload");
-  //     const res = await fetch(presignedurl, {
-  //       method: "PUT",
-  //       body: file,
-  //       headers: {
-  //         "Content-Type": type
-  //       }
-  //     })
-  //     if (res.status === 200) {
-  //     } else {
-  //       console.log(res);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
 
   const onsubmit = async (event) => {
     event.preventDefault();
