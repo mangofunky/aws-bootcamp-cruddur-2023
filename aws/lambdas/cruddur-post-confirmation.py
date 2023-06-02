0060142f0d8240ddd9b8c6821a88577c11d7235c
mangofunky <mangofunky@hotmail.com>
2023-06-02 13:49:54 +0000

    amended lambda post confirmation code

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -2,15 +2,16 @@ import json
 import psycopg2
 import os
 
+
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
     print('userAttributes')
     print(user)
 
-    user_display_name  = user['name']
-    user_email         = user['email']
-    user_handle        = user['preferred_username']
-    user_cognito_id    = user['sub']
+    user_display_name = user['name']
+    user_email = user['email']
+    user_handle = user['preferred_username']
+    user_cognito_id = user['sub']
     try:
       print('entered-try')
       sql = f"""
@@ -20,20 +21,20 @@ def lambda_handler(event, context):
           handle, 
           cognito_user_id
           ) 
-        VALUES(%s,%s,%s,%s)
+        VALUES(%(display_name)s,%(email)s,%(handle)s,%(cognito_user_id)s)
       """
       print('SQL Statement ----')
       print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      params = [
-        user_display_name,
-        user_email,
-        user_handle,
-        user_cognito_id
-      ]
-      cur.execute(sql,*params)
-      conn.commit() 
+      params = {
+          'display_name': user_display_name,
+          'email': user_email,
+          'handle': user_handle,
+          'cognito_user_id': user_cognito_id
+      }
+      cur.execute(sql, params)
+      conn.commit()
 
     except (Exception, psycopg2.DatabaseError) as error:
       print(error)
